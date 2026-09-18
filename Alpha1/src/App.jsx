d7be646893e26d02589a16c7f7c3601ddb113991
Therso11 <thersogames@gmail.com>
2026-09-18 22:53:53 +0200

    Contactos
    Simple panel de contactos

--- Alpha1/src/App.jsx
+++ Alpha1/src/App.jsx
@@ -4,12 +4,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import Home from './componentes/Home'
+import Contactos from './componentes/Contactos'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Home/>
+      <Contactos/>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
